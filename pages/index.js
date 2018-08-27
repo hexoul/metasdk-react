0e47ed7e74eb7c4d78ec868d472a8bff735df1c7
hexoul <crosien@gmail.com>
2018-08-27 16:11:30 +0900

    attach next for test

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TestIndex extends Component {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+        <p>Test page</p>
+    )
+  }
+}
+
+export default TestIndex;
