a652c91516362cb47a40fdaec66111f89ae48e93
hexoul <crosien@gmail.com>
2018-08-30 11:16:49 +0900

    Add initialization about session and URI to Login

--- src/index.js
+++ src/index.js
@@ -3,9 +3,42 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
+const NodeRSA = require('node-rsa');
+
 export default class Login extends Component {
   static propTypes = {
     text: PropTypes.string
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      session: this.makeSessionID(),
+      requestUri: '',
+    };
+  }
+
+  makeSessionID() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+
+  componentDidMount() {
+    const key = new NodeRSA({b: 2048});
+    this.pubkey = key.exportKey('public');
+    this.privkey = key.exportKey('private');
+
+    var pubkey = this.pubkey
+      .replace('-----BEGIN PUBLIC KEY-----', '')
+      .replace('-----END PUBLIC KEY-----', '')
+      .replace(/\s+|\n\r|\n|\r$/gm, '');
+    pubkey = encodeURIComponent(pubkey);
+    
+    this.baseRequestUri = "meta://information?request=name&request=email&service=https%3A%2F%2Fmetastellar.metadium.com&callback=http%3A%2F%2F13.125.251.87%3A3000/metainfo?session=";
+    this.setState({requestUri: this.baseRequestUri + this.state.session + "&public_key=" + pubkey});
   }
 
   render() {
@@ -15,7 +48,8 @@ export default class Login extends Component {
 
     return (
       <div className={styles.test}>
-        Login: {text}
+        Session: {this.state.session != undefined && this.state.session} <br />
+        Uri: {this.state.requestUri != undefined && this.state.requestUri}
       </div>
     )
   }
