380e612c2cce8d2bb82e9b8a064bb6609b68bd3c
hexoul <crosien@gmail.com>
2019-05-21 10:51:06 +0900

    fix: use ipfs-http-client instead of deprecated ipfs-api

--- src/components/Ipfs.js
+++ src/components/Ipfs.js
@@ -1,4 +1,5 @@
-const IPFS = require('ipfs-api')
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+const ipfsClient = require('ipfs-http-client')
+// const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+const ipfs = ipfsClient({ host: '15.164.64.229', port: 5001, protocol: 'http' })
 
 export default ipfs
